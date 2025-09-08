28abec91656e485311db2f7cc705fd239eb6af63
Ajeem Ahmad <Ahmedazim7804@gmail.com>
2025-09-08 12:47:07 +0530

    Create wallpaper.ps1

---
+++ wallpaper.ps1
@@ -0,0 +1,21 @@
+# URL of the wallpaper image to download
+$imageUrl = "https://raw.githubusercontent.com/Ahmedazim7804/random/refs/heads/main/Screenshot_2025-09-08-12-28-50-438_com.linkedin.android.jpg"
+
+# Local path to save the downloaded image
+$localPath = "$env:TEMP\wallpaper.jpg"
+
+# Download the image
+Invoke-WebRequest -Uri $imageUrl -OutFile $localPath
+
+# Add the native function to set the wallpaper
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+public class W {
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}
+"@
+
+# Set the downloaded image as wallpaper
+[W]::SystemParametersInfo(20, 0, $localPath, 3)
